8e1da58841abad17483f87329d6da8805449f681
MohdAyyan <mohdayyan297@gmail.com>
2026-08-04 21:50:31 +0530

    Iinitialize full-stack architecture with Clerk authentication, Strapi CMS integration, and UI components

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 
  import { Cookie, Refrigerator, Sparkles } from "lucide-react";
-import { SignedIn, SignedOut, SignInButton, SignUpButton } from "@clerk/nextjs";
+import { PricingTable, SignedIn, SignedOut, SignInButton, SignUpButton } from "@clerk/nextjs";
 import { Button } from "./ui/button";
 import Link from "next/link";
 import Image from "next/image";
 import UserDropdown from "./UserDropdown";
+import { checkUser } from "@/lib/checkUser";
+import PricingModel from "./PricingModel";
+import { Badge } from "./ui/badge";
 
 
 
 export default async function Header() {
-//   const user = await checkUser();
-    const user = null;
+   const user = await checkUser();
+
   return (
     <header className="fixed top-0 w-full border-b border-stone-200 bg-stone-50/80 backdrop-blur-md z-50 supports-backdrop-filter:bg-stone-50/60">
       <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
@@ -44,6 +47,29 @@ export default async function Header() {
           
 
           <SignedIn>
+            {
+              user && <PricingModel subscriptionTier={user.subscriptionTier}>
+                <Badge  variant="outline"
+                  className={`flex h-8 px-3 gap-1.5 rounded-full text-xs font-semibold transition-all ${
+                    user.subscriptionTier === "pro"
+                      ? "bg-linear-to-r from-orange-600 to-amber-500 text-white border-none shadow-sm"
+                      : "bg-stone-200/50 text-stone-600 border-stone-200 cursor-pointer hover:bg-stone-300/50 hover:border-stone-300"
+                  }`}>
+                   <Sparkles
+                    className={`h-3 w-3 ${
+                      user.subscriptionTier === "pro"
+                        ? "text-white fill-white/20"
+                        : "text-stone-500"
+                    }`}
+                  />
+                   <span>
+                    {
+                    user.subscriptionTier === "pro" ? "Pro Chef" : "Free Plan"
+                    }
+                    </span>
+                </Badge>
+                </PricingModel>
+            }
            <UserDropdown/> 
       </SignedIn>
 
