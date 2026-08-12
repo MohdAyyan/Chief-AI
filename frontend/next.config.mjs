676e4c508a6df71c20693423bfe257f767e37599
MohdAyyan <mohdayyan297@gmail.com>
2026-08-12 21:09:09 +0530

    removed the experimental

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  experimental: {
-    serverComponentsHmrCache: false, // defaults to true
-  },
+
   serverExternalPackages: ["@arcjet/next", "@arcjet/node", "@arcjet/analyze-wasm"],
   images: {
     remotePatterns: [
