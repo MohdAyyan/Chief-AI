4446055aa45dec0e965b353fedbf3e48385d0322
MohdAyyan <mohdayyan297@gmail.com>
2026-08-13 11:57:57 +0530

    added the proxy

--- frontend/proxy.js
+++ frontend/proxy.js
@@ -1,39 +1,32 @@
-import { auth, clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
-import { NextResponse } from 'next/server';
-import { aj } from './lib/arcjet';
-
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 const isProtectedRoute = createRouteMatcher([
-    "/recipe(.*)",
-    "/recipes(.*)",
-    "/pantry(.*)",
-    "/dashboard(.*)",
+  "/recipe(.*)",
+  "/recipes(.*)",
+  "/pantry(.*)",
+  "/dashboard(.*)",
 ]);
 
-export default clerkMiddleware(async(auth, req)=>{
+export default clerkMiddleware(async (auth, req) => {
+  const { userId, redirectToSignIn } = await auth();
 
-  const decision = await aj.protect(req)
-
-  if(decision.isDenied()) {
-    return NextResponse.json({error:"Request Denied"})
+  if (!userId && isProtectedRoute(req)) {
+    return redirectToSignIn();
   }
 
-  const { userId, redirectToSignIn } = await auth()
-    if (!userId && isProtectedRoute(req)) {
-        const { redirectToSignIn } = await auth();
-        return redirectToSignIn();
-      }
-    
-      return NextResponse.next();
+  return NextResponse.next();
 });
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for Clerk's auto-proxy path
-    '/__clerk/:path*',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
+    // Skip Next.js internals and static files
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+
+    // API routes
+    "/(api|trpc)(.*)",
+
+    // Clerk routes
+    "/__clerk/(.*)",
   ],
 };
